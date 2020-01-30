f220c81ef9807f58700e6cc89f048b0a0cf067a7
ricou12 <dwwm2019@gmail.com>
2020-01-30 16:50:25 +0100

    Ca avance à petit pas

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
+// conteneur principal de la page html
 const $todos = document.querySelector('.todos');
+// bouton validation ajout todo
+const $addedTodo = document.querySelector('.addedTodoSave');
+
 
 /* ----------------------------------------------------
   ECOUTE LES EVENEMENTS A PARTIR DE LA BALISE PARENT
@@ -6,38 +10,140 @@ const $todos = document.querySelector('.todos');
 $todos.addEventListener('click', (event) => {
     // Récupère l'élément qui recoit le focus
     const element = event.target;
-  
-    const $listTodo = document.querySelector('.listTodo');
-    if ($listTodo.hasAttribute('data-id')){
-        const id = $listTodo.getAttribute('data-id');
-        if (element.classList.contains('listTodo_title')) {
-            getTodo(id);
-        }
-        // Si on veut modifier
-        if (element.classList.contains('listTodo')) {
-            changeTodo(id);
-        }
-        // si l'on souhaite supprimer
-        if (element.classList.contains('listTodo_trash')) {
-            deleteTodo(id);
+
+
+    // DELETE
+    if (element.classList.contains('listTodo_trash')) {
+        if (element.hasAttribute('data-id')) {
+            const id = element.getAttribute('data-id');
+            deleteTodo(id).then(returnData => {
+                if (returnData) {
+                    getListTodo();
+                    console.log('Suppression réussie');
+                } else {
+                    console.log('Impossible de supprimer');
+                }
+            });
         }
     }
+
+    // GET Affiche le todo 
+    if (element.classList.contains('listTodo_title')) {
+        if (element.hasAttribute('data-id')) {
+            const id = element.getAttribute('data-id');
+            getTodo(id);
+        }
+    }
+
+    // PATCH
+        if (element.classList.contains('listTodo')) {
+            if (element.hasAttribute('data-id')) {
+                const id = element.getAttribute('data-id');
+                changeTodo(id);
+            }
+        }
 });
 
+
 /* ------------------------------------------
-  AFFICHER ET MODIFIER UN TODO
+  RECUPERE LA LISTE DES TODOS ET AFFICHE 
 ------------------------------------------- */
-// afficher un todo
-const getTodo = (id) => {
-    fetch("http://localhost:3000/api/v1/todos/1")
+const getListTodo = () => {
+    fetch("http://localhost:3000/api/v1/todos")
         .then(res => res.json())
-        .then(dataTodo => {
-            console.log(dataTodo);
-            $todos.innerHTML = changeTodo(dataTodo);
+        .then(dataTodos => {
+            console.log(dataTodos);
+            $todos.innerHTML = dataTodos.map(todo => showTodo(todo)).join("");
         });
 }
 
-// Modifier le todo.
+const showTodo = (todo) => {
+    return `
+        <div class="row listTodo border">
+            <div class="col-12 col-lg-5 d-flex flex-nowrap align-items-center p-2">
+                <div class="d-flex">
+                    <input type="checkbox" class="listTodo_done">
+                    <h4 class="p-2 listTodo_Date">${new Date(todo.createdAt).toLocaleString()}</h4>
+                </div>
+                <a href="#"><h4 class="listTodo_title" data-id="${todo.id}">${todo.title}</h4></a>
+            </div>
+            <div class="col-12 col-lg-7 d-flex flex-nowrap justify-content-between align-items-center p-2">
+                <h5 class="listTodo_content">${todo.content}</h5>
+                 <a href="#"><img class="border border-danger rounded listTodo_trash" src="./img/trash.png" alt="trash" data-id="${todo.id}" width="50"></a>
+            </div>
+        </div>
+   `
+}
+
+/* ----------------------------------------------------
+                AJOUTER UN TODO
+----------------------------------------------------- */
+$addedTodo.addEventListener('click', () => {
+    const title = document.querySelector('.addedTodoTitle');
+    const content = document.querySelector('.addedTodoContent');
+    const data = {
+        "title": title.value,
+        "content": content.value
+    };
+    added(data).then(data => {
+        // getListTodo();
+        // Ajout le nouveau todo à la liste
+        $todos.innerHTML += showTodo(data);
+        // Vide les champs input.
+        title.value ="";
+        content.value ="";
+    });
+
+});
+
+const added = (data) => {
+    return fetch("http://localhost:3000/api/v1/todos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(returnData => {
+            return returnData;
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
+
+/* ------------------------------------------
+        AFFICHER ET MODIFIER UN TODO
+------------------------------------------- */
+// afficher un todo
+const getTodo = (id) => {
+    fetch(`http://localhost:3000/api/v1/todos/${id}`)
+        .then(res => res.json())
+        .then(dataTodo => {
+            console.log(dataTodo);
+            $todos.innerHTML = showTodoDetail(dataTodo);
+        });
+}
+// Affiche tout le contenu du todo.
+const showTodoDetail = (todo) => {
+    return `
+    <div class="container border bg-light">
+        <div class="row changeTodo">
+                <div class="col-12 d-flex align-items-center">
+                    <input type="checkbox" class="showTodoDetail_done">
+                    <h4 class="p-2 listTodo_Date">${new Date(todo.createdAt).toLocaleString()}</h4>
+                    <h4 type="text" class="mx-2 showTodoDetail_title">${todo.title}</h4>
+                </div>
+                <div class="col-12 d-flex align-items-center">
+                    <input type="text" class="mx-2 showTodoDetail_content" value="${todo.content}">
+                    <a href="#" class="btn btn-primary showTodoDetail_btnChange float-right">Retour</a>
+                </div>   
+        </div>
+    <div>`
+}
+
+// Affiche tout le contenu du todo.
 const changeTodo = (todo) => {
     return `
     <div class="container border">
@@ -49,7 +155,7 @@ const changeTodo = (todo) => {
                     <input type="text" class="mx-2 changeTodo_content" value="${todo.content}">
                 </div>
                 <div class="d-flex">
-                    <a href="#" class="btn btn-primary changeTodo_btnChange float-right"  data-id="${todo.id}">Modifier</a>
+                    <a href="#" class="btn btn-primary changeTodo_btnChange float-right" data-id="${todo.id}">Modifier</a>
                 </div>   
             </div>
         </div>
@@ -57,39 +163,22 @@ const changeTodo = (todo) => {
 }
 
 /* ------------------------------------------
-  RECUPERE LA LISTE DES TODOS ET AFFICHE 
+                DELETE UN TODO
 ------------------------------------------- */
-const getListTodo = () => {
-    fetch("http://localhost:3000/api/v1/todos")
-    .then(res => res.json())
-    .then(dataTodos => {
-        console.log(dataTodos);
-        $todos.innerHTML = dataTodos.map(todo => generateTodo(todo)).join("") ;
-    });
+const deleteTodo = (id) => {
+    return fetch(`http://localhost:3000/api/v1/todos/${id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(returnData => {
+            return returnData;
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
 
-const generateTodo = (todo) => {
-    return `
-    <div class="container border">
-        <div class="row listTodo" data-id="${todo.id}">
-            <div class="col-12 col-lg-5 d-flex flex-nowrap align-items-center p-2">
-                <div class="d-flex">
-                    <input type="checkbox" class="listTodo_done">
-                    <h4 class="p-2 listTodo_Date">${todo.createdAt}</h4>
-                </div>
-                <a href="#"><h4 class="listTodo_title">${todo.title}</h4></a>
-            </div>
-            <div class="col-12 col-lg-7 d-flex flex-nowrap justify-content-between align-items-center p-2">
-                <h5 class="listTodo_content">${todo.content}</h5>
-                 <a href="#"><img class="border border-danger rounded listTodo_trash" src="./img/trash.png" alt="trash" data-id="${todo.id}" width="50"></a>
-            </div>
-        </div>
-   <div>
-   `   
-}
-
-
-
+/* ------------------------------------------
+            CHARGEMENT DE LA PAGE
+------------------------------------------- */
 getListTodo();
-
-
